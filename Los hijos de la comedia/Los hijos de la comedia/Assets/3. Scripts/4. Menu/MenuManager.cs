f3fee08247ad5f6bff1b231aa3bcbcb988960533
DouglasVargas123 <d.vargasvsquez1@uandresbello.edu>
2023-11-20 16:47:55 -0300

    12
    dsfid

--- Los hijos de la comedia/Los hijos de la comedia/Assets/3. Scripts/4. Menu/MenuManager.cs
+++ Los hijos de la comedia/Los hijos de la comedia/Assets/3. Scripts/4. Menu/MenuManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MenuManager : MonoBehaviour
 {
     private Animator anim;
+    public EventSystem eventSystem;
+    public GameObject[] botones;
 
     private void Start()
     {
@@ -14,26 +17,31 @@ public class MenuManager : MonoBehaviour
     public void SwitchMenu()
     {
         anim.SetTrigger("SwitchMenu");
+        eventSystem.firstSelectedGameObject = botones[0]; //Reanudar
     }
 
     public void SwitchGameplay()
     {
         anim.SetTrigger("SwitchGameplay");
+        eventSystem.firstSelectedGameObject = null;
     }
 
     public void SwitchOptions()
     {
         anim.SetTrigger("SwitchOpciones");
+        eventSystem.firstSelectedGameObject = botones[1]; //Controles
     }
 
     public void SwitchBestiario()
     {
         anim.SetTrigger("SwitchBestiario");
+        eventSystem.firstSelectedGameObject = botones[2]; //Volver Bestiario
     }
 
     public void SwitchGuardarYSalir()
     {
         anim.SetTrigger("SwitchSalir");
+        eventSystem.firstSelectedGameObject = botones[3]; //Opcion si
     }
 
     public void SwitchOpcionesAmenu()
